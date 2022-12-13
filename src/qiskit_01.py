58ff140779342c240243f6746e9dc63e1d7e7be3
Emmanuel Watila <ei.watila@gmail.com>
2022-12-13 20:41:11 +0100

    Initial commit

---
+++ src/qiskit_01.py
@@ -0,0 +1,16 @@
+from qiskit import QuantumCircuit, transpile
+from qiskit_aer.backends import AerSimulator
+
+simulator = AerSimulator()
+
+circ = QuantumCircuit(2, 2)
+circ.h([0, 1])
+circ.cx(0, 1)
+circ.measure([0, 1], [0, 1])
+compiled_circ = transpile(circ, simulator)
+
+job = simulator.run(compiled_circ)
+results = job.result()
+counts = results.get_counts()
+
+print(counts)
