bb80373fd24bcac677a187f19b3a1131458520ad
Emmanuel Watila <ei.watila@gmail.com>
2022-12-13 22:35:24 +0100

    Wrote comments for src

--- src/qiskit_01.py
+++ src/qiskit_01.py
@@ -1,16 +1,24 @@
 from qiskit import QuantumCircuit, transpile
-from qiskit_aer.backends import AerSimulator
+from qiskit.providers.basicaer import QasmSimulatorPy
 
-simulator = AerSimulator()
+# Setup Simulator - QasmSimulatorPy
+simulator = QasmSimulatorPy()
 
+
+# Design a circuit with a 2 qubit register and 2 bit classical register
+# Put qubit [0] and [1] in superposition using Hadamard gate
+# Entangle qubit [0] to [1] using the CX (CNOT) gate
+# Measure outcome on respective classical register
 circ = QuantumCircuit(2, 2)
 circ.h([0, 1])
 circ.cx(0, 1)
 circ.measure([0, 1], [0, 1])
 compiled_circ = transpile(circ, simulator)
 
+# Set up job on QasmSimulator and get the result
 job = simulator.run(compiled_circ, shots=1024)
 results = job.result()
 counts = results.get_counts()
 
+# Print the counts of probabilities
 print(counts)
